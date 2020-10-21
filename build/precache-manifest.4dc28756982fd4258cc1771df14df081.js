963dcc9b995380a536431de7ca700e21524c3cc4
javier <javiermontes1997@hotmail.com>
2020-10-21 20:35:20 +0200

    creación de bundle en entorno de producción

---
+++ build/precache-manifest.4dc28756982fd4258cc1771df14df081.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14268746640bcef7e9f64ac22bbf1bf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba868f0da85ea94aef6f",
+    "url": "/static/css/main.d7334abb.chunk.css"
+  },
+  {
+    "revision": "2ef54bb5d2dc904c4fd8",
+    "url": "/static/js/2.c217b3ba.chunk.js"
+  },
+  {
+    "revision": "3c56d32f1a0e992453f57615992b86b8",
+    "url": "/static/js/2.c217b3ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba868f0da85ea94aef6f",
+    "url": "/static/js/main.ce78e574.chunk.js"
+  },
+  {
+    "revision": "be447dada72c318b2c30",
+    "url": "/static/js/runtime-main.7c8c4bf3.js"
+  },
+  {
+    "revision": "53ef8ed8ef4b92721f00cb8bfdc5c9b9",
+    "url": "/static/media/icon-arrow.53ef8ed8.svg"
+  },
+  {
+    "revision": "ff17146919959b508ee5dd9af2f9f9e5",
+    "url": "/static/media/icon-location.ff171469.svg"
+  },
+  {
+    "revision": "8d3f4ad64bf3a9320e172edf01e0c016",
+    "url": "/static/media/pattern-bg.8d3f4ad6.png"
+  }
+]);
